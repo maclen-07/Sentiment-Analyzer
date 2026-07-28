e0065789b3f19332320b9d1beca28b0cc4b01490
maclen-07 <emailfortrials.07@gmail.com>
2026-07-28 10:35:16 +0530

    Add sentiment analysis app with Gradio interface
    Implement a sentiment analysis application using Gradio and Transformers.

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+import gradio as gr
+from transformers import pipeline
+
+classifier = pipeline(
+    "sentiment-analysis",
+    # model="distilbert-base-uncased-finetuned-sst-2-english"
+    model="cardiffnlp/twitter-roberta-base-sentiment-latest"
+)
+
+def analyze_sentiment(text):
+    # Handle empty input
+    if not text.strip():
+        return "Please enter some text.", ""
+
+    result = classifier(text)
+
+    label = result[0]["label"]
+    score = f"{result[0]['score']:.2f}"
+
+    return label, score
+
+app = gr.Interface(
+    fn=analyze_sentiment,
+    inputs=gr.Textbox(
+        label="Enter a Sentence",
+        lines=3,
+        placeholder="Type a sentence here..."
+    ),
+    outputs=[
+        gr.Textbox(label="Sentiment"),
+        gr.Textbox(label="Score")
+    ],
+    title="Sentiment Analyzer by Surya :D",
+    description="Enter a sentence to analyze its sentiment."
+)
+
+app.launch(server_name="0.0.0.0", server_port=int(OS.environ.get("PORT",7860)))
